7705e03b1a5f3475d3c92ad769d96a39d4d4e841
danielvanc <email@danielvanc.com>
2021-08-10 12:52:16 +0100

    chore: Added a bunch of libraries

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
 module.exports = {
+  // TODO: Re-enable webpack5 once issue with Tailwind is resolved.
+  // Issue is that tailwind doesn't work on 5.
+  webpack5: false,
   reactStrictMode: true,
 }
