6179a090b767c5e9b3903ddb5eab1540aca709c0
danielvanc <email@danielvanc.com>
2021-08-16 18:49:23 +0100

    set up studio path

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
+const STUDIO_REWRITE = {
+  source: "/studio/:path*",
+  destination:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3333/studio/:path*"
+      : "/studio/index.html",
+};
+
 module.exports = {
   // TODO: Re-enable webpack5 once issue with Tailwind is resolved.
   // Issue is that tailwind doesn't work on 5.
   webpack5: false,
   reactStrictMode: true,
+  rewrites: () => [STUDIO_REWRITE],
 }
