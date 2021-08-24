038a7924206e3efa066cc225c98d7962592ba0be
Daniel Van Cuylenburg <email@danielvanc.com>
2021-08-24 17:33:56 +0100

    feat: Added base theme defaults to tailwind

---
+++ src/pages/_document.tsx
@@ -0,0 +1,21 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+          <link href="https://fonts.googleapis.com/css2?family=Fredoka+One&family=Montserrat:wght@600&family=Source+Serif+Pro:wght@400;700&display=swap" rel="stylesheet"></link>
+        </Head>
+        <body className="font-serifPro">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,26 @@ module.exports = {
   purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        rosyWorm: {
+          900: '#81334F',
+          DEFAULT: '#610023'
+        },
+        grayWorm: {
+          100: '#EEE7E1',
+          200: '#EAE2DA',
+          800: '#D9D1C9',
+          DEFAULT: '#393131'
+        },
+        peachy: '#DDCCC8'
+      },
+    },
+    fontFamily: {
+      fred: ['"Fredoka One"', 'cursive'],
+      monty: ['Montserrat', 'sans-serif'],
+      serifPro: ['"Source Serif Pro"', 'serif']
+    }
   },
   variants: {
     extend: {},
