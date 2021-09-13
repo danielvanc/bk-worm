34145e9506fa3d68264c02c90425b9485698ef5d
Daniel Van Cuylenburg <email@danielvanc.com>
2021-09-13 12:09:18 +0100

    chore: Added jest mock for nexts dynamic imports

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,1 +1,12 @@
-import '@testing-library/jest-dom/extend-expect'
+import "@testing-library/jest-dom/extend-expect";
+
+jest.mock("next/dynamic", () => (func) => {
+  let component = null;
+  func().then((module) => {
+    component = module.default;
+  });
+  const DynamicComponent = (...args) => component(...args);
+  DynamicComponent.displayName = "LoadableComponent";
+  DynamicComponent.preload = jest.fn();
+  return DynamicComponent;
+});
