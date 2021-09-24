c5f8e671c55190aa58f589fdf27fc9c330f5ec71
Daniel Van Cuylenburg <email@danielvanc.com>
2021-09-24 11:32:44 +0100

    feat: grid set up on small mobiles for unauth page

--- src/components/unauthenticated.tsx
+++ src/components/unauthenticated.tsx
@@ -6,19 +6,24 @@ import { useAuth } from "context/auth";
 export default function Unauthenticated() {
   const { session } = useAuth();
   return (
-    <div className="grid grid-cols-8 gap-3 lm:grid-cols-12 lm:gap-4">
-      <Image src="/img/logo.svg" alt="" width="131" height="38" />
-      <header className="uppercase text-grayWorm-300">
-        <h1 className="opacity-5 leading-tighter lm:leading-10 lm:opacity-100  font-fred text-hMedium lm:text-hSmall desktop:text-hLarge desktop:leading-none">
+    <div className="grid grid-cols-8 gap-3 lm:grid-cols-12 lm:gap-4 auto-rows-auto">
+      <div className="col-start-2 col-span-2 row-start-1 row-span-1">
+        <Image src="/img/logo.svg" alt="" width="131" height="38" />
+      </div>
+
+      <header className="grid grid-cols-8 gap-3 uppercase text-grayWorm-300 col-span-full row-start-2 row-span-1">
+        <h1 className="col-start-1 col-span-6 row-start-1 row-span-full opacity-5 leading-tighter lm:leading-10 lm:opacity-100  font-fred text-hMedium lm:text-hSmall desktop:text-hLarge desktop:leading-none">
           Read more
         </h1>
-        <h2 className="font-fred text-grayWorm-300 text-sm lm:text-base desktop:text-h2Normal desktop:leading-3">
+        <h2 className="font-fred text-grayWorm-300 text-sm lm:text-base desktop:text-h2Normal desktop:leading-3 col-start-2 col-span-5 row-start-1 row-end-2 self-center">
           Find read and spread your love for books
         </h2>
-        <p className="text-sm lm:text-xl">Let your mind escape</p>
+        <p className="font-fred text-grayWorm-300 text-sm lm:text-xl col-start-2 col-span-5 row-start-1 row-end-2 self-end">
+          Let your mind escape
+        </p>
       </header>
 
-      <main>
+      <main className="row-start-3 row-span-1 col-start-2 col-span-5">
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
           bibendum suspendisse purus netus est mauris. Morbi vivamus rutrum
